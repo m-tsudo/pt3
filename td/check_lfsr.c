8dea93824a222d40b9b393752c63d7cabee32ca9
m-tsudo <anyone>
2012-07-28 12:35:24 +0900

    GPLの文言追加

--- td/check_lfsr.c
+++ td/check_lfsr.c
@@ -1,3 +1,20 @@
+/*******************************************************************************
+   earthsoft PT3 Linux driver
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms and conditions of the GNU General Public License,
+   version 3, as published by the Free Software Foundation.
+
+   This program is distributed in the hope it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+   more details.
+
+   The full GNU General Public License is included in this distribution in
+   the file called "COPYING".
+
+ *******************************************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
